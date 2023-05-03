37497a862462af9c50829a157cffdd7f76e14ca7
DiogoDSJ <107954198+DiogoDSJ@users.noreply.github.com>
2023-05-02 21:33:44 -0300

    Update mainteste.v

--- mainteste.v
+++ mainteste.v
@@ -1,18 +1,14 @@
-module main(ch1,ch0,clk,saida_reg1,saida_reg2,saida_reg3,saida_reg4,saida_reg5,acender_coluna,linha1,linha2,linha3,linha4,linha5,clock1,clock2);
+module main(ch1,ch0,clk,saida_reg1,saida_reg2,saida_reg3,saida_reg4,saida_reg5,acender_coluna);
     input clk,ch1,ch0;
+	 output saida_reg1,saida_reg2,saida_reg3,saida_reg4,saida_reg5;
 	 output [6:0] acender_coluna;
-	 output [15:0] linha1;
-	 output [15:0] linha2;
-	 output [15:0] linha3;
-	 output [15:0] linha4;
-	 output [15:0] linha5;
-	 output [6:0] saida_reg1;
-	 output [6:0] saida_reg2;
-	 output [6:0] saida_reg3;
-	 output [6:0] saida_reg4;
-	 output [6:0] saida_reg5;
-	 output clock1;
-	 output clock2;
+	 wire [15:0] linha1;
+	 wire [15:0] linha2;
+	 wire [15:0] linha3;
+	 wire [15:0] linha4;
+	 wire [15:0] linha5;
+	 wire clock1;
+	 wire clock2;
 	 // clk = clock de 50mhz recebido pela FPGA.
 	 // ch1 e ch0 são as chaves de seleção.
 	 // saida_reg1 até saida_reg5 são as saidas últimas saidas dos flipflops do registrador de 7 bits.
@@ -31,11 +27,11 @@ module main(ch1,ch0,clk,saida_reg1,saida_reg2,saida_reg3,saida_reg4,saida_reg5,a
 	 registrador reg_4(0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,1,clock2, ch1, ch0, linha4);
 	 registrador reg_5(0,1,1,1,0,0,0,1,0,1,1,1,0,1,1,1,clock2, ch1, ch0, linha5);
 	 
-	 registrador_7bits reg7b_1(linha1[15:9], clock1, saida_reg1,acender_coluna[6]);
-	 registrador_7bits reg7b_2(linha2[15:9], clock1, saida_reg2,acender_coluna[6]);
-	 registrador_7bits reg7b_3(linha3[15:9], clock1, saida_reg3,acender_coluna[6]);
-	 registrador_7bits reg7b_4(linha4[15:9], clock1, saida_reg4,acender_coluna[6]);
-	 registrador_7bits reg7b_5(linha5[15:9], clock1, saida_reg5,acender_coluna[6]);
+	 registrador_7bits reg7b_1(linha1[6:0], clock1, saida_reg1,acender_coluna[6]);
+	 registrador_7bits reg7b_2(linha2[6:0], clock1, saida_reg2,acender_coluna[6]);
+	 registrador_7bits reg7b_3(linha3[6:0], clock1, saida_reg3,acender_coluna[6]);
+	 registrador_7bits reg7b_4(linha4[6:0], clock1, saida_reg4,acender_coluna[6]);
+	 registrador_7bits reg7b_5(linha5[6:0], clock1, saida_reg5,acender_coluna[6]);
 	 
 	 
 endmodule
